b88edd282526379cf24d1bd6c8b2aa68ff8a6139
Ross Dooney <rossdooney@gmail.com>
2025-03-26 23:57:12 +0000

    a

--- main.go
+++ main.go
@@ -16,29 +16,40 @@ func main() {
 		return
 	}
 
-	txtConent := ""
-	for {
-		b := make([]byte, 8)
-		n, err := data.Read(b)
+	linesChan := getLinesChannel(data)
 
-		if err != nil && !errors.Is(err, io.EOF) {
-			fmt.Println(err)
-			break
-		}
-
-		if err != nil {
-			break
-		}
-		str := string(b[:n])
-		txtConent += str
+	for line := range linesChan {
+		fmt.Println("read:", line)
 	}
+}
 
-	stuff := strings.Split(txtConent, "\n")
-	for i := 0; i < len(stuff); i++ {
-		if stuff[i] == "" {
-			break
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(lines)
+		currentLineContents := ""
+		for {
+			b := make([]byte, 8, 8)
+			n, err := f.Read(b)
+			if err != nil {
+				if currentLineContents != "" {
+					lines <- currentLineContents
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				return
+			}
+			str := string(b[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
+				currentLineContents = ""
+			}
+			currentLineContents += parts[len(parts)-1]
 		}
-		fmt.Printf("read: %v \n", stuff[i])
-	}
-
+	}()
+	return lines
 }
