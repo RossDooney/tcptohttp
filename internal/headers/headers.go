17464ae83e90f212fa8a004213f3f726e46fd67b
Ross Dooney <rossdooney@gmail.com>
2025-06-10 00:14:10 +0100

    updated to only act on one header line at a time

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -6,92 +6,39 @@ import (
 	"strings"
 )
 
+const crlf = "\r\n"
+
 type Headers map[string]string
 
 func NewHeaders() Headers {
-	return make(Headers)
+	return map[string]string{}
 }
-
-const crlf = "\r\n"
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
-
-	done = false
-	bytesRead := 0
-	for !done {
-		n, err = parseRequestLine(data)
-		if err != nil {
-			return 0, false, err
-		}
-		if n == 0 {
-			done = true
-			break
-		}
-		requestLineText := string(data[:n])
-		key, value, err := headerFromString(requestLineText)
-
-		if err != nil {
-			return 0, false, err
-		}
-
-		h[key] = value
-		// n + 2 to include \r\n, another +2 at the end to include the last escape char
-		bytesRead += n + 2
-		data = data[n+2:]
-
-	}
-
-	return bytesRead, done, nil
-
-}
-
-func parseRequestLine(data []byte) (int, error) {
 	idx := bytes.Index(data, []byte(crlf))
 	if idx == -1 {
-		return 0, nil
+		return 0, false, nil
 	}
-	return idx, nil
+	if idx == 0 {
+		// the empty line
+		// headers are done, consume the CRLF
+		return 2, true, nil
+	}
+
+	parts := bytes.SplitN(data[:idx], []byte(":"), 2)
+	key := string(parts[0])
+
+	if key != strings.TrimRight(key, " ") {
+		return 0, false, fmt.Errorf("invalid header name: %s", key)
+	}
+
+	value := bytes.TrimSpace(parts[1])
+	key = strings.TrimSpace(key)
+
+	h.Set(key, string(value))
+	return idx + 2, false, nil
 }
 
-func headerFromString(str string) (string, string, error) {
-	before, after, _ := strings.Cut(str, ":")
-
-	start := 0
-	for _, c := range before {
-		if c != ' ' && c != '\t' {
-			break
-		}
-		start++
-	}
-
-	key := before[start:]
-	for _, c := range key {
-		if c == ' ' || c == '\t' {
-			return "", "", fmt.Errorf("Whitespace present in header key")
-		}
-	}
-
-	start = 0
-	end := len(after) - 1
-
-	isASCIISpace := func(c byte) bool {
-		return c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\v' || c == '\f'
-	}
-
-	for start <= end && isASCIISpace(after[start]) {
-		start++
-	}
-	for end >= start && isASCIISpace(after[end]) {
-		end--
-	}
-
-	value := after[start : end+1]
-
-	for _, c := range value {
-		if c == ' ' || c == '\t' {
-			return "", "", fmt.Errorf("Whitespace present in header value")
-		}
-	}
-
-	return key, value, nil
+func (h Headers) Set(key, value string) {
+	h[key] = value
 }
