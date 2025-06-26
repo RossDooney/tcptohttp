8a259f109f801ef80f3c7861d80f581eb7292777
RossDooney <rossdooney@googlemail.com>
2025-06-26 17:33:49 +0100

    handler updated

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"bytes"
+	"fmt"
 	"httpTest/internal/request"
 	"httpTest/internal/response"
 	"io"
@@ -14,17 +16,42 @@ type HandlerError struct {
 
 type Handler func(w io.Writer, req *request.Request) *HandlerError
 
+func (handleErr HandlerError) Write(w io.Writer) {
+	errMsg := fmt.Sprintf("HTTP/1.1 %d %s\r\n", handleErr.StatusCode, handleErr.StatusMsg)
+
+	w.Write([]byte(errMsg))
+}
+
 func (s *Server) handle(conn net.Conn) {
 	defer conn.Close()
-	err := response.WriteStatusLine(conn, 200)
+
+	req, err := request.RequestFromReader(conn)
+
+	if err != nil {
+		handleErr := &HandlerError{
+			StatusCode: 400,
+			StatusMsg:  err.Error(),
+		}
+		handleErr.Write(conn)
+		return
+	}
+
+	buf := bytes.NewBuffer([]byte{})
+	handleErr := s.handler(buf, req)
+
+	if handleErr != nil {
+		handleErr.Write(conn)
+	}
+
+	b := buf.Bytes()
+	err = response.WriteStatusLine(conn, 200)
 
 	if err != nil {
 		return
 	}
 
-	headers := response.GetDefaultHeaders(0)
+	headers := response.GetDefaultHeaders(len(b))
 	err = response.WriteHeaders(conn, headers)
-
 	if err != nil {
 		return
 	}
